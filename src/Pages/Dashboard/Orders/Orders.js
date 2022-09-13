3d3b2cb42bc59a0e0242c9ac8e5b4b8c47aa5892
unknown <shahidmonowar.cse@gmail.com>
2022-09-14 00:20:12 +0600

    send jwt to backend, verify & handle unauthorize access

--- src/Pages/Dashboard/Orders/Orders.js
+++ src/Pages/Dashboard/Orders/Orders.js
@@ -1,17 +1,36 @@
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { Table } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 const Orders = () => {
     const [orders, setOrders] = useState([]);
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
 
     useEffect(()=>{
         if(user){
-            fetch(`http://localhost:5000/order?email=${user.email}`)
-        .then(res=> res.json())
-        .then(data => setOrders(data));
+            fetch(`http://localhost:5000/order?email=${user.email}`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+        .then(res=> {
+            console.log('res',res);
+            if(res.status === 401 || res.status === 403){
+                signOut(auth);
+    localStorage.removeItem('accessToken');
+                navigate('/');
+            }
+            return res.json();
+        })
+        .then(data => {
+            
+            setOrders(data);
+        });
         }
     },[user])
     return (
