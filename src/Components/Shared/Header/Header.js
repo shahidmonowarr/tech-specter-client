e4bc61eeb66c8e753f65abf1d9ca41670da83efc
unknown <shahidmonowar.cse@gmail.com>
2022-09-23 17:41:47 +0600

    header edited

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -1,50 +1,68 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
-import { HashLink } from 'react-router-hash-link';
-import auth from '../../../firebase.init';
-import logo from '../../../images/logo.png';
-import './Header.css';
+import { signOut } from "firebase/auth";
+import React from "react";
+import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+import auth from "../../../firebase.init";
+import logo from "../../../images/logo.png";
+import "./Header.css";
 
 const Header = () => {
   const [user] = useAuthState(auth);
 
-  const logOut = ()=>{
+  const logOut = () => {
     signOut(auth);
-    localStorage.removeItem('accessToken');
-  }
+    localStorage.removeItem("accessToken");
+  };
 
-    return (
-        <Navbar style={{ backgroundColor: "#1c2331" }} variant='dark'sticky='top' expand="lg">
+  return (
+    <Navbar
+      style={{ backgroundColor: "#1c2331" }}
+      variant="dark"
+      sticky="top"
+      expand="lg"
+    >
       <Container>
         <Navbar.Brand as={HashLink} to="/home">
-            <div className='logo'>
+          <div className="logo">
             <img width="100%" src={logo} alt="logo" />
-            </div>
+          </div>
         </Navbar.Brand>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
         <Navbar.Collapse data-toggle="offcanvas">
           <Nav className="ms-auto">
-            <Nav.Link as={HashLink} to="/home#home">Home</Nav.Link>
-            <Nav.Link as={HashLink} to="/services">Services</Nav.Link>
-            {
-              user && <Nav.Link as={HashLink} to="/dashboard">Dashboard</Nav.Link>
-            }
-            {user ?
+            <Nav.Link as={HashLink} to="/home#home">
+              Home
+            </Nav.Link>
+            <Nav.Link as={HashLink} to="/services">
+              Services
+            </Nav.Link>
+            {user && (
+              <Nav.Link as={HashLink} to="/dashboard">
+                Dashboard
+              </Nav.Link>
+            )}
+            {/* {user ?
                                 <Navbar.Text>
                                 <a style={{ textDecoration: "none" }} href="#login"> {user?.displayName}</a>
                             </Navbar.Text>
                                 :
                                 <Nav.Link as={Link} to="/login"></Nav.Link>}
-            {user ? <Button variant="outline-success" onClick={logOut} type="button" className="btn btn-light ms-2">Sign Out</Button>: <Nav.Link as={HashLink} to="/login">Login</Nav.Link>}
+            {user ? <Button variant="outline-success" onClick={logOut} type="button" className="btn btn-light ms-2">Sign Out</Button>: <Nav.Link as={HashLink} to="/login">Login</Nav.Link>} */}
+
+           {
+            user? ( <NavDropdown className="text-uppercase fw-bold shadow-lg" title={user?.displayName} id="collasible-nav-dropdown">
+              
+            <NavDropdown.Item><Button onClick={logOut} type="button" className="btn btn-dark rounded-3 px-auto">Sign Out</Button></NavDropdown.Item>
             
+          </NavDropdown>):(<Nav.Link className="fw-bold shadow-sm" as={HashLink} to="/login">Login</Nav.Link>)
+           }
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 };
 
 export default Header;
--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -13,54 +13,6 @@ const Dashboard = () => {
     <div className="container">
       <div className="row">
         <div className="col-md-2 pt-2 my-auto shadow-lg">
-          {/* <ul className="">
-            <li className="me-3 my-2 rounded-3 btn-dark">
-              <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-                Home
-              </Link>
-            </li>
-            <li className="me-3 my-2 rounded-3 btn-dark">
-              <Link
-                to="/dashboard/addReview"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                Add Review
-              </Link>
-            </li>
-            <li className="me-3 my-2 rounded-3 btn-dark">
-              <Link
-                to="/dashboard/myOrders"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                My Orders
-              </Link>
-            </li>
-            {admin && <li className="me-3 my-2 rounded-3 btn-dark">
-              <Link
-                to="/dashboard/users"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                All Users
-              </Link>
-            </li>}
-            {admin && <li className="me-3 my-2 rounded-3 btn-dark">
-              <Link
-                to="/dashboard/addCourse"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                Add Course
-              </Link>
-            </li>}
-            {admin && <li className="me-3 my-2 rounded-3 btn-dark">
-              <Link
-                to="/dashboard/manageOrders"
-                style={{ textDecoration: "none", color: "white" }}
-              >
-                Manage Orders
-              </Link>
-            </li>}
-          </ul> */}
-
           {admin ? (
             <ul>
               <li className="me-3 my-2 rounded-3 btn-dark">
