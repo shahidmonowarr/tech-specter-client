45a218a169a390c733f7f866e2ba770077d640ed
shahidmonowarr <shahidmonowar21@gmail.com>
2022-11-12 20:48:57 +0600

    istructor details added

--- src/Pages/CoursesPage/CourseDetails/CourseDetails.js
+++ src/Pages/CoursesPage/CourseDetails/CourseDetails.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import "./CourseDetails.css";
 import { Col, Container, Row, Toast } from "react-bootstrap";
-import Rating from "react-rating";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
-import useCourseDetails from "../../../hooks/useCourseDetails/useCourseDetails";
+import Rating from "react-rating";
+import { Link, useParams } from "react-router-dom";
 import PageTitle from "../../../Components/Shared/PageTitle/PageTitle";
+import useCourseDetails from "../../../hooks/useCourseDetails/useCourseDetails";
+import "./CourseDetails.css";
 
 const CourseDetails = () => {
   const { serviceId } = useParams();
@@ -51,6 +50,45 @@ const CourseDetails = () => {
             src={course.image}
             style={{ height: "auto" }}
           />
+          <Card style={{ width: "auto" }}>
+            <Card.Title style={{ paddingTop: "20px", marginLeft: '5px' }}>
+              Instructor Details:
+            </Card.Title>
+            <Row>
+              <Col>
+                <Card.Img
+                  style={{ width: "100%", marginTop: "80px", marginLeft: "10px" }}
+                  src="https://i.ibb.co/m83YDnf/elegant-young-teacher-with-brunette-hair-stylish-light-shirt-black-striped-suit-glasses-holding-writ.jpg"
+                />
+              </Col>
+              <Col>
+                <Card.Body>
+                  <Card.Text className="fw-bold">Junior Ahmed</Card.Text>
+                  <small style={{ color: "lightBlack" }}>
+                    A technology entrepreneur. He himself is a full stack web
+                    developer and software architect and has been involved in
+                    the web development and software profession for over 14
+                    years.
+                  </small>
+                  <br />
+                  <Card.Text style={{ fontWeight: "bold", color: "red" }}>
+                    <small>juniorahmed.cse@gmail.com</small>{" "}
+                    <small>Phone: +880 1711 000 000</small>{" "}
+                    <small>
+                      Reviews:{" "}
+                      <Rating
+                        className="text-warning"
+                        emptySymbol="far fa-star"
+                        fullSymbol="fas fa-star"
+                        initialRating="4.5"
+                        readonly
+                      />
+                    </small>
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
           <Card className="my-2">
             <h6 className="fw-bold ms-1 mt-1">Reviews</h6>
             <Row>
@@ -63,9 +101,13 @@ const CourseDetails = () => {
                   md={3}
                   lg={3}
                 >
-                    <Card style={{ width: 'auto' }} className="ps-3 py-2">
+                  <Card
+                    style={{ width: "auto", height: "140px" }}
+                    className="ps-3 py-2"
+                  >
+                    <small>
                       <h6>{review.name}</h6>
-                      <h6>
+                      <p>
                         <Rating
                           className="text-warning"
                           emptySymbol="far fa-star"
@@ -74,9 +116,10 @@ const CourseDetails = () => {
                           readonly
                         />{" "}
                         {review.rating}
-                      </h6>
+                      </p>
                       <small>{review.description}</small>
-                    </Card>
+                    </small>
+                  </Card>
                 </Col>
               ))}
             </Row>
@@ -91,7 +134,13 @@ const CourseDetails = () => {
               {course.name}
             </Card.Header>
             <h6 className="rating">
-              <Rating defaultValue={5} fullSymbol="fas fa-star" />
+            <Rating
+                          className="text-warning pb-2 mt-2"
+                          emptySymbol="far fa-star"
+                          fullSymbol="fas fa-star"
+                          initialRating="4.5"
+                          readonly
+                        />{" "}
               <br />
               <small>based on 129 reviews</small>
             </h6>
@@ -111,7 +160,9 @@ const CourseDetails = () => {
                 <li>Commitment to deliver your project on time</li>
               </ul>
               <Link to={`/checkOut/${serviceId}`}>
-                <Button className="me-2" variant="danger">Order Now</Button>
+                <Button className="me-2" variant="danger">
+                  Enroll Now
+                </Button>
               </Link>
               <Link to="/dashboard/addReview">
                 <Button className="mt-2" variant="danger">
