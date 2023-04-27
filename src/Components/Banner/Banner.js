450cc7f607ce1ec00bb91a25f2bddf53695b91dc
shahidmonowarr <shahidmonowar21@gmail.com>
2023-04-27 15:09:17 +0600

    updated banner

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,45 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import PageTitle from "../Shared/PageTitle/PageTitle";
 import "./Banner.css";
-
+const imageUrl = "https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80";
 const Banner = () => {
-  return (
-    <div className="banner">
-      <PageTitle title="Home"></PageTitle>
-      <h1 className="text-center">
-        FIND YOUR <br /> SOLUTIONS HERE
-      </h1>
-      <Link to="/services">
-        {" "}
-        <button className="bannerBtn">Explore Now</button>
-      </Link>
-      <div class="custom-shape-divider-bottom-1665992396">
-        <svg
-          data-name="Layer 1"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1200 120"
-          preserveAspectRatio="none"
-        >
-          <path
-            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            class="shape-fill"
-          ></path>
-        </svg>
+  return ( 
+    <div style={{height: "100vh", backgroundImage: `url(${imageUrl})`, backgroundSize: "cover", backgroundPosition: "center"}} className="position-relative w-100"><PageTitle title="Home"></PageTitle>
+    <div className="position-absolute text-white d-flex flex-column align-items-start justify-content-center" style={{top: "0", right: "0", bottom: "0", left: "0", backgroundColor: "rgba(0,0,0,.7)"}}>
+      <div className="container">
+        <div className="col-md-6">
+          <span style={{color: "#bbb"}} className="text-uppercase">Hello There...</span>
+          <h1 className="mb-4 mt-2 display-4 font-weight-bold">Find Your</h1>
+          <h1 className="mb-4 mt-2 display-5 font-weight-bold"><span style={{color: "#9B5DE5"}}>Solutions Here</span></h1>
+          <p  style={{color: "#bbb"}}>We are providing best solutions here</p>
+          <div className="mt-5">
+            <Link to="/services" className="btn px-5 py-3 text-white mt-3 mt-sm-0" style={{borderRadius: "30px", backgroundColor: "#9B5DE5"}}>Get Started</Link>
+          </div>
+        </div>
       </div>
-      {/* <div>
-          <h1 className="text-center">
-          FIND YOUR <br/> SOLUTIONS HERE
-          </h1>
-          <div className="text-center">
-            <Link to="/services">
-              {" "}
-              <button className="btn btn-warning text-dark">
-                Explore Now
-              </button>
-            </Link>
-          </div>{" "}
-        </div> */}
     </div>
+  </div>
   );
 };
 
